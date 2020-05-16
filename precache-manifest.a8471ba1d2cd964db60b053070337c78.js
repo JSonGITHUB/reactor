d23c24170fd92c00735e5eb11c8d217fece454fc
John Santucci <jhnsntcc@gmail.com>
2020-05-16 16:39:45 -0700

    Updates

--- precache-manifest.a8471ba1d2cd964db60b053070337c78.js
+++ precache-manifest.a8471ba1d2cd964db60b053070337c78.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7914cc301813b2beb4114963df41f45",
+    "revision": "a301e8a16d3f5e6713b8e2402885b599",
     "url": "/reactor/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactor/static/css/2.93b3b32d.chunk.css"
   },
   {
-    "revision": "c6f052c63fa1e1ab4623",
-    "url": "/reactor/static/css/main.8718e9f1.chunk.css"
+    "revision": "d95ac2cd2a6556921c95",
+    "url": "/reactor/static/css/main.48e4b6e7.chunk.css"
   },
   {
     "revision": "cca5f825bd92433af8c6",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactor/static/js/2.0a5e8232.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c6f052c63fa1e1ab4623",
-    "url": "/reactor/static/js/main.c2d944e8.chunk.js"
+    "revision": "d95ac2cd2a6556921c95",
+    "url": "/reactor/static/js/main.979247e4.chunk.js"
   },
   {
     "revision": "89efb22c44a6ab3e0839",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactor/precache-manifest.9dbb7c55a43ff1ddd80609a04473e4b5.js"
+  "/reactor/precache-manifest.a8471ba1d2cd964db60b053070337c78.js"
 );
 
 self.addEventListener('message', (event) => {
