82b35258bb12431c008d920d4b2b91d0b16eb354
John Santucci <jhnsntcc@gmail.com>
2020-05-18 09:41:00 -0700

    layout fix

--- src/components/WaveFinder.js
+++ src/components/WaveFinder.js
@@ -605,13 +605,12 @@ class WaveFinder extends React.Component {
                                 </label>
                             </div>
                         </div>
-                        <br/>
-                        <div className="ml-20 mr-20">
+                        <div className="m-30">
                             <span className="color-neogreen bold">{count} waves</span> out of {locations.length}<br/>
                             are in a <span className="color-neogreen bold">{this.state.distance}</span> mile radius<br/>
                             and prefer <span className="color-neogreen bold">{swell1Direction} </span>and <span className="color-orange bold ">{swell2Direction} </span>swell with <span className="color-neogreen bold">{tide} </span>tide:
                         </div>
-                        <br/><br/>
+                        <br/>
                         {getLocations}
                     </div>     
                 </Dialog>  
