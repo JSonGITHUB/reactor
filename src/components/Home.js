1842db68067bfeab5f820adc112c0b23b9d799d9
John Santucci <jhnsntcc@gmail.com>
2020-05-18 20:09:55 -0700

    track distance feet

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,7 +35,8 @@ class Home extends React.Component {
             dist = Math.acos(dist);
             dist = dist * 180/Math.PI;
             dist = dist * 60 * 1.1515;
-            dist = dist.toFixed(0);
+            dist = dist;
+            dist = dist*5280
             if (unit==="Kilometers") { dist = dist * 1.609344 }
             if (unit==="Nautical") { dist = dist * 0.8684 }
             console.log(`DISTANCE => ${dist}`)
