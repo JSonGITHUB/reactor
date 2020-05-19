6d5e16772cbcf461989b0f181dc93bb49ec5c706
John Santucci <jhnsntcc@gmail.com>
2020-05-18 19:29:32 -0700

    track distance

--- src/components/WaveFinder.js
+++ src/components/WaveFinder.js
@@ -544,32 +544,32 @@ class WaveFinder extends React.Component {
         const windDirectionMatch = (direction) => (direction.wind === windDirection) ? true : false;
         const tideDirectionMatch = (direction) => (direction.tide === tide) ? true : false;
         const calculateDistance = (item) => {
-                const lat1 = item.latitude;
-                const lat2 = this.state.latitude;
-                const lon1 = item.longitude;
-                const lon2 = this.state.longitude;
-                const unit = "Miles"
+            const lat1 = item.latitude;
+            const lat2 = this.state.latitude;
+            const lon1 = item.longitude;
+            const lon2 = this.state.longitude;
+            const unit = "Miles"
 
-                if ((lat1 === lat2) && (lon1 === lon2)) {
-                    return 0;
-                }else {
-                    var radlat1 = Math.PI * lat1/180;
-                    var radlat2 = Math.PI * lat2/180;
-                    var theta = lon1-lon2;
-                    var radtheta = Math.PI * theta/180;
-                    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-                    if (dist > 1) {
-                        dist = 1;
-                    }
-                    dist = Math.acos(dist);
-                    dist = dist * 180/Math.PI;
-                    dist = dist * 60 * 1.1515;
-                    dist = dist.toFixed(0);
-                    if (unit==="Kilometers") { dist = dist * 1.609344 }
-                    if (unit==="Nautical") { dist = dist * 0.8684 }
-                    console.log(`DISTANCE => ${dist}`)
-                    return dist;
+            if ((lat1 === lat2) && (lon1 === lon2)) {
+                return 0;
+            }else {
+                var radlat1 = Math.PI * lat1/180;
+                var radlat2 = Math.PI * lat2/180;
+                var theta = lon1-lon2;
+                var radtheta = Math.PI * theta/180;
+                var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+                if (dist > 1) {
+                    dist = 1;
                 }
+                dist = Math.acos(dist);
+                dist = dist * 180/Math.PI;
+                dist = dist * 60 * 1.1515;
+                dist = dist.toFixed(0);
+                if (unit==="Kilometers") { dist = dist * 1.609344 }
+                if (unit==="Nautical") { dist = dist * 0.8684 }
+                console.log(`DISTANCE => ${dist}`)
+                return dist;
+            }
         }
         const distance = (item) => calculateDistance(item);
         //Math.abs(item.latitude - this.state.latitude)+Math.abs(item.longitude - this.state.longitude);
