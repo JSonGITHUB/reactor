80b2d21d0ef44c5e6a12ff6f4c38af9e80e19025
John Santucci <jhnsntcc@gmail.com>
2020-07-27 17:59:55 -0700

    local swell

---
+++ src/components/waves/Locations.js
@@ -0,0 +1,370 @@
+export default function getLocations() {
+    return [{
+        "name": "HB: 17th St.",
+        "latitude": 33.663781,
+        "longitude": -118.013605,
+        "swell": ["SSE", "S", "SW", "WSW", "W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "HB: Taco Bell Reef",
+        "latitude": 33.657999,
+        "longitude": -118.006578,
+        "swell": ["SSE", "S", "SW", "WSW", "W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "HB: North Peir",
+        "latitude": 33.655927,
+        "longitude": -118.003874,
+        "swell": ["SSE", "S", "SW", "WSW", "W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "HB: South Peir",
+        "latitude": 33.655534,
+        "longitude": -118.003145,
+        "swell": ["SSE", "S", "SW", "WSW", "W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "HB: River Jetties",
+        "latitude": 33.630302,
+        "longitude": -117.961721,
+        "swell": ["SSE", "S", "SW","W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium", "high"]
+    },
+    {
+        "name": "Salt Creek",
+        "latitude": 33.475456,
+        "longitude": -117.722133,
+        "swell": ["S", "SW", "W", "WNW"],
+        "wind": ["E", "SE", "S"],
+        "tide": ["medium", "high"]
+    },
+    {
+        "name": "Lowers",
+        "latitude": 33.382848,
+        "longitude": -117.588214,
+        "swell": ["S", "SW","W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "O-Side: Harbor North",
+        "latitude": 33.206684,
+        "longitude": -117.397452,
+        "swell": ["SSW", "SW", "W", "WNW", "SSE"],
+        "wind": ["E"],
+        "tide": ["medium", "high"]
+    },
+    {
+        "name": "O-Side: Harbor South",
+        "latitude": 33.202483,
+        "longitude": -117.392796,
+        "swell": ["SSW", "SW", "W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["medium", "high"]
+    },
+    {
+        "name": "O-Side: Pier North",
+        "latitude": 33.194686,
+        "longitude": -117.385226,
+        "swell": ["SSW", "SW", "W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "O-Side: Pier South",
+        "latitude": 33.193630,
+        "longitude": -117.384826,
+        "swell": ["SSW", "SW", "W", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["medium", "high"]
+    },
+    {
+        "name": "Carlsbad",
+        "latitude": 33.144850,
+        "longitude": -117.343638,
+        "swell": ["WNW", "W", "SW", "SSW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Ponto: Jetties",
+        "latitude": 33.086801,
+        "longitude": -117.313695,
+        "swell": ["W", "NW", "SW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Sea Bluff",
+        "latitude": 33.081980,
+        "longitude": -117.311783,
+        "swell": ["W", "NW", "SW", "SSW", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Grandview",
+        "latitude": 33.076397,
+        "longitude": -117.310334,
+        "swell": ["W", "NW", "SW", "SSW", "WNW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Beacons",
+        "latitude": 33.065118,
+        "longitude": -117.305518,
+        "swell": ["W", "NW", "SW", "SSW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "D Street",
+        "latitude": 33.046486,
+        "longitude": -117.298161,
+        "swell": ["W", "WNW", "NW", "SW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Swamis",
+        "latitude": 33.034592,
+        "longitude": -117.292734,
+        "swell": ["W", "NW"],
+        "wind": ["E"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Pipes",
+        "latitude": 33.026892,
+        "longitude": -117.287915,
+        "swell": ["W", "NW", "SW"],
+        "wind": ["E", "NE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Traps",
+        "latitude": 33.025580,
+        "longitude": -117.287165,
+        "swell": ["NW","W"],
+        "wind": ["E"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Cardiff Reef",
+        "latitude": 33.015631,
+        "longitude": -117.282085,
+        "swell": ["NW","W"],
+        "wind": ["E"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Georges",
+        "latitude": 33.010952,
+        "longitude": -117.280085,
+        "swell": ["NW","W"],
+        "wind": ["E"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Seaside Reef",
+        "latitude": 33.001613,
+        "longitude": -117.278393,
+        "swell": ["NW","W"],
+        "wind": ["E"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Del Mar",
+        "latitude": 32.976395,
+        "longitude": -117.270974,
+        "swell": ["SW", "W", "NW"],
+        "wind": ["E"],
+        "tide": ["low", "medium", "high"]
+    },
+    {
+        "name": "Torrey Pines",
+        "latitude": 32.938600,
+        "longitude": -117.261978,
+        "swell": ["SW", "WNW", "NW"],
+        "wind": ["NE", "E", "SE"],
+        "tide": ["medium", "high"]
+    },
+    {
+        "name": "Blacks",
+        "latitude": 32.881882,
+        "longitude": -117.252467,
+        "swell": ["W","NW", "SW"],
+        "wind": ["E", "SE"],
+        "tide": ["low", "medium"]
+    },
+    {
+        "name": "Scripps",
+        "latitude": 32.865358,
+        "longitude": -117.254981,
+        "swell": ["W", "NW", "SW"],
+        "wind": ["E", "SE", "S"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "La Jolla Shores",
+        "latitude": 32.858424,
+        "longitude": -117.256791,
+        "swell": ["W", "NW"],
+        "wind": ["E"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "Mission Beach",
+        "latitude": 32.767649,
+        "longitude": -117.252731,
+        "swell": ["W", "NW", "SW"],
+        "wind": ["E", "S"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "OB Jetti",
+        "latitude": 32.754755,
+        "longitude": -117.253815,
+        "swell": ["W", "NW", "SW"],
+        "wind": ["E"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "OB Avalanche",
+        "latitude": 32.751873,
+        "longitude": -117.252972,
+        "swell": ["W", "NW", "SW"],
+        "wind": ["E"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "OB Pier",
+        "latitude": 32.747869,
+        "longitude": -117.253615,
+        "swell": ["W", "NW", "SW"],
+        "wind": ["E"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "Sunset Cliffs",
+        "latitude": 32.725570,
+        "longitude": -117.258111,
+        "swell": ["W", "NW", "SW"],
+        "wind": ["E"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "Rosarito",
+        "latitude": 32.333760,
+        "longitude": -117.056838,
+        "swell": ["S", "SW"],
+        "wind": ["E"],
+        "tide": ["medium"]
+    },
+    {
+        "name": "K-38s",
+        "latitude": 32.259594,
+        "longitude": -116.987307,
+        "swell": ["S", "SW", "W", "WNW"],
+        "wind": ["NE","E"],
+        "tide": ["medium", "low"]
+    },
+    {
+        "name": "Gaviotas",
+        "latitude": 32.252500,
+        "longitude": -116.961600,
+        "swell": ["S", "SW", "W", "WNW"],
+        "wind": ["NE", "ENE", "E", "ESE", "SE"],
+        "tide": ["high", "medium", "low"]
+    },
+    {
+        "name": "La Fonda",
+        "latitude": 32.121058,
+        "longitude": -116.885713,
+        "swell": ["SW", "W"],
+        "wind": ["E"],
+        "tide": ["medium", "low"]
+    },
+    {
+        "name": "Punta Baja",
+        "latitude": 29.954293,
+        "longitude": -115.807737,
+        "swell": ["SW", "SSW", "S", "WNW", "W"],
+        "wind": ["N", "NE"],
+        "tide": ["high", "medium", "low"]
+    },
+    {
+        "name": "Elijandros",
+        "latitude": 28.706507,
+        "longitude": -114.288678,
+        "swell": ["W", "WNW", "NW"],
+        "wind": ["N","NE", "E", "NW"],
+        "tide": ["high", "medium", "low"]
+    },
+    {
+        "name": "Harbor",
+        "latitude": 28.666795,
+        "longitude": -114.239317,
+        "swell": ["WNW", "NW"],
+        "wind": ["N"],
+        "tide": ["medium", "low"]
+    },
+    {
+        "name": "Notch",
+        "latitude": 28.666800,
+        "longitude": -114.224431,
+        "swell": ["WNW", "NW"],
+        "wind": ["N"],
+        "tide": ["medium", "low"]
+    },
+    {
+        "name": "Wall",
+        "latitude": 28.566481,
+        "longitude": -114.158590,
+        "swell": ["W", "WNW", "NW"],
+        "wind": ["N", "NE", "ENE"],
+        "tide": ["high", "medium", "low"]
+    },
+    {
+        "name": "Abreojos",
+        "latitude": 26.722327,
+        "longitude": -113.546932,
+        "swell": ["S"],
+        "wind": ["N", "NE", "NW"],
+        "tide": ["high", "medium"]
+    },
+    {
+        "name": "Scorpion Bay",
+        "latitude": 26.239488,
+        "longitude": -112.477709,
+        "swell": ["SW","SSW"],
+        "wind": ["N", "NW"],
+        "tide": ["medium", "low"]
+    },
+    {
+        "name": "Estuary",
+        "latitude": 23.050415,
+        "longitude": -109.678612,
+        "swell": ["S", "SE"],
+        "wind": ["N"],
+        "tide": ["high", "medium", "low"]
+    },
+    {
+        "name": "Colorados",
+        "latitude": 11.406466,
+        "longitude": -86.048310,
+        "swell": ["SW"],
+        "wind": ["E", "NE"],
+        "tide": ["high", "medium"]
+    }]
+}
