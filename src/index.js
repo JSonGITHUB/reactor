1292af0c1571393a60147a19ef23a0f3f322c75f
John Santucci <jhnsntcc@gmail.com>
2021-01-14 18:24:52 -0800

    index to App css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/index.css';
+import './assets/css/App.css';
 //import App from './AppNoRouter';
 import App from './App';
 
