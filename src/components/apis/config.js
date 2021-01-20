0483c84385f89ae8eaae27070aa61d89c0f9067b
John Santucci <jhnsntcc@gmail.com>
2021-01-20 10:13:10 -0800

    console log removal and translate key

--- src/components/apis/config.js
+++ src/components/apis/config.js
@@ -4,6 +4,8 @@ const config = {
     openweatherAPI_KEY: '5d7ac91cc224722d3eec83598909e1dc',
     openweatherAPI_BASE_URL: 'https://api.openweathermap.org',
     youtubeAPI_KEY: 'AIzaSyDxITGx4VJUa4_nUnhJJqwa790l5ICifNU',
-    youtubeAPI_BASE_URL: 'https://www.googleapis.com/youtube/v3'
+    youtubeAPI_BASE_URL: 'https://www.googleapis.com/youtube/v3',
+    googleAPI_KEY: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+    googleAPI_BASE_URL:'https://translation.googleapis.com/language/translate/v2'
 }
 export default config
