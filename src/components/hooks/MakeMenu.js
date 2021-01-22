8cac7067f70d7f3a12fd4a39ddab33a75ce847a5
John Santucci <jhnsntcc@gmail.com>
2021-01-22 11:02:10 -0800

    Updates

--- src/components/hooks/MakeMenu.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function MakeMenu(props) {
-    const makeMenu = (menu) => menu.map(item => <div>{item}</div>);
-    const menuData = ["one","two","thee"];
-    return (
-        <div>{makeMenu(menuData)}</div>
-    )
-}
--- src/components/site/NavClasses.js
+++
@@ -1,6 +0,0 @@
-const navClasses = 'width-100-percent navigation mt-2 pointer bg-dark'
-const navClassesClosed = navClasses + ' faded';
-const navClassesOpen = navClasses + ' fadeInFaded fadedDark';
-const navClassesClose  = navClasses + ' fadeOutFaded faded';
-
-export { navClassesClosed, navClassesOpen, navClassesClose };
