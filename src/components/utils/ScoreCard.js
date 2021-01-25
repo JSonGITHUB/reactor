74add167de600e577fdddbd8ae2b4fb3b46193db
John Santucci <jhnsntcc@gmail.com>
2021-01-25 15:54:43 -0800

    ScoreKeeper mobile layout

--- src/components/utils/ScoreCard.js
+++ src/components/utils/ScoreCard.js
@@ -7,22 +7,22 @@ const ScoreCard = ({ player, index, editPlayer, deletePlayer }) => {
     const addScore = () => setScore(score+1);
     const subtractScore = () => setScore(score-1);
     return (
-        <div className='r-10 bg-dkGreen p-10 m-1 mb-10'>
-            {player}:
-            <div className="flexContainer width-100-percent mt-5">
+        <div className='r-10 bg-dkGreen m-1 mb-10 pb-20'>
+            <div className='color-yellow p-10 bg-dkGreen r-5 mb-20'>{player}:</div>
+            <div className="flexContainer mt-5">
                 <div className="flex3Column">
-                    <div className='button bg-red p-20 m-20 r-10' onClick={() => subtractScore()}>-</div>
-                    <div className='button bg-red p-20 m-20 r-10' onClick={() => deletePlayer(index)}>DELETE</div>
+                    <div className='button bg-red p-10 m-10 r-10' onClick={() => subtractScore()}>-</div>
+                    <div className='button bg-red p-10 m-10 r-10' onClick={() => deletePlayer(index)}>DELETE</div>
                 </div>
                 <input
-                    className='m-5 flex3Column App bg-dark bold white navBranding'
+                    className='flex3Column App bg-dark white'
                     type="text"
                     value={score}
                     onChange={e => handleChange}
                 />
                 <div className="flex3Column">
-                    <div className='button bg-green p-20 m-20 r-10' onClick={() => addScore()}>+</div>
-                    <div className='button bg-green p-20 m-20 r-10' onClick={() => editPlayer(index)}>EDIT</div>
+                    <div className='button bg-green p-10 m-10 r-10' onClick={() => addScore()}>+</div>
+                    <div className='button bg-green p-10 m-10 r-10' onClick={() => editPlayer(index)}>EDIT</div>
                 </div>
             </div>
         </div>
