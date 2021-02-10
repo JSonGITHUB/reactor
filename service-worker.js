fb8d75c6c42e35077b44ba1af51811dd000a0afe
John Santucci <jhnsntcc@gmail.com>
2021-02-09 17:43:11 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactor/precache-manifest.60ed62cc8d7cdd63b85603e7a505fdc0.js"
+  "/reactor/precache-manifest.4d2c654753331dff150f2f62971c860a.js"
 );
 
 self.addEventListener('message', (event) => {
