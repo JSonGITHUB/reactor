08cdeae2d3b8aab1ad1cb46d91f43f8e488d9d43
John Santucci <jhnsntcc@gmail.com>
2021-02-12 09:10:37 -0800

    Updates

---
+++ precache-manifest.491643f96e8218c8fd1cc0fe022651f3.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8dd19f5e628595bd2b328ef95216af",
+    "url": "/reactor/index.html"
+  },
+  {
+    "revision": "84fe119aac8c2fc076d0",
+    "url": "/reactor/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "dabd9b23c8b29f1480e6",
+    "url": "/reactor/static/css/main.bdbfb650.chunk.css"
+  },
+  {
+    "revision": "84fe119aac8c2fc076d0",
+    "url": "/reactor/static/js/2.26ba0b2f.chunk.js"
+  },
+  {
+    "revision": "43c72baf7326c0879f6f10dc8debfe2c",
+    "url": "/reactor/static/js/2.26ba0b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabd9b23c8b29f1480e6",
+    "url": "/reactor/static/js/main.0ec2b604.chunk.js"
+  },
+  {
+    "revision": "3ab5db8ed9368c243238",
+    "url": "/reactor/static/js/runtime-main.04744323.js"
+  },
+  {
+    "revision": "50adf984d5d79c2425fd8b72be02d625",
+    "url": "/reactor/static/media/ArrowUp.50adf984.png"
+  },
+  {
+    "revision": "b1ede02ae9e009065b8ff82fc0c9ff07",
+    "url": "/reactor/static/media/IMG_2034.b1ede02a.JPG"
+  },
+  {
+    "revision": "4277b28953b32d07e4a1d9a8736039f4",
+    "url": "/reactor/static/media/IMG_2049.4277b289.JPG"
+  },
+  {
+    "revision": "fdb902cd49e1729a9f68e056277466bf",
+    "url": "/reactor/static/media/IMG_2060.fdb902cd.JPG"
+  },
+  {
+    "revision": "fd04eb44e23cfb5bee6d5d526d7c062b",
+    "url": "/reactor/static/media/IMG_2061.fd04eb44.JPG"
+  },
+  {
+    "revision": "e48c93d3eaaaf120ad5c8332ea92a5ca",
+    "url": "/reactor/static/media/IMG_2062.e48c93d3.JPG"
+  },
+  {
+    "revision": "4a862f2128bb77e3db3d5d6af206f66a",
+    "url": "/reactor/static/media/IMG_2063.4a862f21.JPG"
+  },
+  {
+    "revision": "f3a22065d58a229733d5ccfda874d243",
+    "url": "/reactor/static/media/airTemp.f3a22065.png"
+  },
+  {
+    "revision": "77bfacf1f0153215251a4f8622252106",
+    "url": "/reactor/static/media/logoGreen.77bfacf1.png"
+  },
+  {
+    "revision": "bc675d065f7f68c1973494fd4639aa1d",
+    "url": "/reactor/static/media/logoRed.bc675d06.png"
+  },
+  {
+    "revision": "cb49daaad8d25b3334172cd098263a4a",
+    "url": "/reactor/static/media/logoYellow.cb49daaa.png"
+  },
+  {
+    "revision": "1bd3ad753a38ed2e895a29e33995753c",
+    "url": "/reactor/static/media/shakaBlack.1bd3ad75.png"
+  },
+  {
+    "revision": "9f80a921b82dc19d825a8365b70ec849",
+    "url": "/reactor/static/media/tide.9f80a921.png"
+  },
+  {
+    "revision": "51e6319f3ea67500c1f321f8515a425c",
+    "url": "/reactor/static/media/waterTemp.51e6319f.png"
+  },
+  {
+    "revision": "dba81acc0a9c5955272a6ce13c840f89",
+    "url": "/reactor/static/media/wavePrimary.dba81acc.png"
+  },
+  {
+    "revision": "f7bca30fc96452afeada49eb944a0af5",
+    "url": "/reactor/static/media/waveSecondaryB.f7bca30f.png"
+  },
+  {
+    "revision": "9badfd1aa289ac82fa23f6a750f1f479",
+    "url": "/reactor/static/media/windE.9badfd1a.png"
+  },
+  {
+    "revision": "7bc2327a1532fa3b6ae00b2e2dd494d5",
+    "url": "/reactor/static/media/windN.7bc2327a.png"
+  },
+  {
+    "revision": "ea0f8e8226c6518c40810ebae183937d",
+    "url": "/reactor/static/media/windNE.ea0f8e82.png"
+  },
+  {
+    "revision": "6c081ea132dfbe9ddad95866f885a9fc",
+    "url": "/reactor/static/media/windNW.6c081ea1.png"
+  },
+  {
+    "revision": "ca9c6d0ce655a6e06189eeee99a3ccd8",
+    "url": "/reactor/static/media/windS.ca9c6d0c.png"
+  },
+  {
+    "revision": "aeb7a072870f243851ff16d09c3ede5e",
+    "url": "/reactor/static/media/windSE.aeb7a072.png"
+  },
+  {
+    "revision": "14fea23569b80840e8b42a6541223cd5",
+    "url": "/reactor/static/media/windSW.14fea235.png"
+  },
+  {
+    "revision": "ee91a07b0a5923fcde63c59389e2b263",
+    "url": "/reactor/static/media/windW.ee91a07b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactor/precache-manifest.b1df42d6914c9fd338e00d99148fdd04.js"
+  "/reactor/precache-manifest.491643f96e8218c8fd1cc0fe022651f3.js"
 );
 
 self.addEventListener('message', (event) => {
