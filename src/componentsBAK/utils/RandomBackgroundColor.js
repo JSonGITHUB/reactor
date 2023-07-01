4585e08acb03fc383a75352769d9ce18b4930cd5
John Santucci <john.santucci@socialedgeconsulting.com>
2023-07-01 14:22:43 -0700

    Updates

--- src/componentsBAK/utils/RandomBackgroundColor.js
+++
@@ -1,6 +0,0 @@
-const randomBackgroundColor = () => {
-    const bgArray = ['bg-dkRed', 'bg-dkYellow', 'bg-dkGreen', 'bg-dkRed', 'bg-dkYellow', 'bg-dkGreen', 'bg-dkRed', 'bg-dkYellow', 'bg-dkGreen', 'bg-dkRed', 'bg-dkYellow', 'bg-dkGreen'];
-    const randomColor = bgArray[((Math.random()*10).toFixed(0))];
-    return randomColor;
-}
-export default randomBackgroundColor;
--- src/componentsBAK/utils/RemoveDuplicateNumbers.js
+++
@@ -1,7 +0,0 @@
-//Uniq Array
-const RemoveDuplicatesNumbers = (numbers) => {
-  numbers = numbers || [1, 1, 2, 1, 3, 4, 1, 5];
-  const uniqNumbers = [...new Set(numbers)];
-  return uniqNumbers;
-};
-export default RemoveDuplicatesNumbers;
