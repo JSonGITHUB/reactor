8c9bf72a7290420fc14143b0e2aa1691c1d3b48c
John Santucci <john.santucci@socialedgeconsulting.com>
2023-07-01 14:22:43 -0700

    Updates

--- src/componentsBAK/utils/RandomColor.js
+++
@@ -1,6 +0,0 @@
-const randomColor = () => {
-    const colorArray = ['color-red', 'color-yellow', 'color-neogreen', 'white', 'color-dark', 'color-lite', 'color-orange', 'color-black', 'color-blue', 'color-graphite']
-    const randomColor = colorArray[((Math.random()*10).toFixed(0))];
-    return randomColor;
-}
-export default randomColor;
