bae166d22b314394bfd28703d92f9471c94dc745
John Santucci <john.santucci@socialedgeconsulting.com>
2023-12-18 12:54:54 -0800

    Updates

--- src/components/hooks/MusicActions.js
+++
@@ -1,9 +0,0 @@
-const selectSong = (song) => {
-    const action = {
-        type: 'SONG_SELECTED',
-        payload: song
-    }
-    console.log(`selectSong =>\naction: ${JSON.stringify(action ,null,2)}`);
-    return action;
-}
-export { selectSong }
--- src/components/site/NavClasses.js
+++
@@ -1,6 +0,0 @@
-const navClasses = 'width-100-percent navigation mt-2 bg-darker'
-const navClassesClosed = navClasses + ' faded';
-const navClassesOpen = navClasses + ' fadeInFaded fadedDark';
-const navClassesClose  = navClasses + ' fadeOutFaded faded';
-
-export { navClassesClosed, navClassesOpen, navClassesClose };
